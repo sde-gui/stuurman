dc6a9bad343d8f5d22c399dc7a1ca69f54884968
Vadim Ushakov <igeekless@gmail.com>
2013-07-13 13:51:09 +0800

    allow automake 1.14

--- autogen.sh
+++ autogen.sh
@@ -8,9 +8,10 @@ AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[
 if [ "$AM_INSTALLED_VERSION" != "1.10" \
     -a "$AM_INSTALLED_VERSION" != "1.11" \
     -a "$AM_INSTALLED_VERSION" != "1.12" \
-    -a "$AM_INSTALLED_VERSION" != "1.13" ];then
+    -a "$AM_INSTALLED_VERSION" != "1.13" \
+    -a "$AM_INSTALLED_VERSION" != "1.14" ];then
 	echo
-	echo "You must have automake 1.10...1.13 installed."
+	echo "You must have automake 1.10...1.14 installed."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	exit 1
