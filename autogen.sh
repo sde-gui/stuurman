b889087d354c2005cbffed0afbb927d4b667dafe
Vadim Ushakov <igeekless@gmail.com>
2015-02-07 12:17:43 +0700

    allow automake 1.15

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,17 @@
-#! /bin/sh
+#!/bin/sh
 AC_VERSION=
 
 AUTOMAKE=${AUTOMAKE:-automake}
 AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[0-9]*\).*/\1/')
 
 # FIXME: we need a better way for version check later.
-if [ "$AM_INSTALLED_VERSION" != "1.10" \
-    -a "$AM_INSTALLED_VERSION" != "1.11" \
+if [ "$AM_INSTALLED_VERSION" != "1.11" \
     -a "$AM_INSTALLED_VERSION" != "1.12" \
     -a "$AM_INSTALLED_VERSION" != "1.13" \
-    -a "$AM_INSTALLED_VERSION" != "1.14" ];then
+    -a "$AM_INSTALLED_VERSION" != "1.14" \
+    -a "$AM_INSTALLED_VERSION" != "1.15" ];then
 	echo
-	echo "You must have automake 1.10...1.14 installed."
+	echo "You must have automake 1.11...1.15 installed."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	exit 1
@@ -23,6 +23,7 @@ if [ "x${ACLOCAL_DIR}" != "x" ]; then
   ACLOCAL_ARG=-I ${ACLOCAL_DIR}
 fi
 
+libtoolize
 ${ACLOCAL:-aclocal$AM_VERSION} ${ACLOCAL_ARG}
 ${AUTOHEADER:-autoheader$AC_VERSION} --force
 AUTOMAKE=$AUTOMAKE intltoolize -c --automake --force
